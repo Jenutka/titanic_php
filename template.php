3f4f785bdbe7b7b97855e724fae4d4beb8f1d13b
Jenutka <honza.prokes@gmail.com>
2023-03-19 16:10:17 +0100

    initial commit

---
+++ template.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>Our Funky HTML Page</title>
+<meta name="description" content="Our first page">
+<meta name="keywords" content="html tutorial template">
+</head>
+<body>
+Content goes here!
+<?php
+
+# require __DIR__ . '/vendor/autoload.php';
+
+echo "Hello";
+
+?>
+</body>
+</html>
